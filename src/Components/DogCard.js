46bf027f2aef2f08f12b57b2061deafc8df94d31
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-03-12 20:46:15 -0400

    Done.

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,15 +1,28 @@
 import React from "react";
 
 class DogCard extends React.Component {
+
+state={
+  clicked: false
+}
+
+clickHandle=()=>{
+  this.setState({
+    clicked: !this.state.clicked
+  })
+}
+
   render() {
     return (
       <div>
-        <h2>{/*Dog name goes here*/}</h2>
-        <img alt="" src={""} />
-        <button onClick={""}>Bark</button>
+        <h2>{this.props.dog.name}</h2>
+        <img alt="" src={this.props.dog.img} />
+        <button onClick={this.clickHandle}>Bark</button>
+        <h1 className='bark'>{this.state.clicked ? "RUFF": ""}</h1>
       </div>
     );
   }
 }
+
 
 export default DogCard;
--- src/Containers/DogsList.js
+++ src/Containers/DogsList.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import DogsData from '../dogs'
+import DogCard from '../Components/DogCard'
+
+
 
 class DogList extends Component {
+
+  state ={
+    dogs: []
+  };
+
+  componentDidMount(){
+      this.setState({dogs: DogsData})
+  }
+
   render() {
-    return <div className="dogContainer">{/*list of dogs go here*/}</div>;
+    const arrayOfDogCards = this.state.dogs.map((dogObj)=> 
+      <DogCard key={dogObj.id} dog={dogObj}/>
+    )
+    return <div className="dogContainer">
+              {this.state.dogs.length > 0 ? arrayOfDogCards : <h1>Loading...</h1>}
+            </div>
   }
 }
 
